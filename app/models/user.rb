1206751111f3dba3ec8530c30650b2396a212bde
enriquer2 <ecases@alfatecsistemas.es>
2017-08-07 14:46:26 +0200

    create after_initialize to set default value

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	after_initialize :set_default_value
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,4 +10,10 @@ class User < ApplicationRecord
     validates :role, presence: true
 
     enum user_roles: { admin: 0, boss: 1, teamleader: 2, employee: 3 }
+
+    protected
+
+    def set_default_value
+      role = self.role || 3
+    end
 end
