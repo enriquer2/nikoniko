9503aac290e7176c0507bd19f040b1f0a77e1efb
enriquer2 <ecases@alfatecsistemas.es>
2017-08-08 10:45:28 +0200

    refactor spec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,5 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-describe User do
-  it 'Should have role 3 as default' do
-    user = User.new
-    expect(user.role).to eq(3)
-  end
+  it { expect(User.new.role).to eq(3) }
 end
