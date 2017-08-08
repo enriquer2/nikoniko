4edb69112d186908e2b42149f2e3dd2267166d43
enriquer2 <ecases@alfatecsistemas.es>
2017-08-08 09:49:16 +0200

    add user test with default value

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,3 +3,10 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { expect(User.new.role).to eq(3) }
 end
+
+describe User do
+  it "Should have role 3 as default" do
+    user = User.new
+    expect(user.role).to eq(3)
+  end
+end
