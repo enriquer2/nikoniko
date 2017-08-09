03188047b55f06eecc1dc910e0b3c495498fdc8d
enriquer2 <ecases@alfatecsistemas.es>
2017-08-09 12:37:12 +0200

    write user factories and user controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -42,37 +42,63 @@ RSpec.describe UsersController, type: :controller do
   # UsersController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
+  let(:user_admin) { create :user_admin }
+  let(:user_boss) { build :user_boss }
+  let(:user_employer) { build :user_employer }
+
+
   describe 'GET #index' do
     it 'returns a success response' do
-      user = User.create! valid_attributes
-      get :index, params: {}, session: valid_session
+      user_admin
+      get :index
       expect(response).to be_success
     end
   end
 
   describe 'GET #show' do
     it 'returns a success response' do
-      user = User.create! valid_attributes
-      get :show, params: { id: user.to_param }, session: valid_session
+      user_admin
+      get :show, { id: user_admin.to_param }
       expect(response).to be_success
     end
   end
 
   describe 'GET #new' do
     it 'returns a success response' do
-      get :new, params: {}, session: valid_session
+      get :new
       expect(response).to be_success
     end
   end
 
   describe 'GET #edit' do
     it 'returns a success response' do
-      user = User.create! valid_attributes
-      get :edit, params: { id: user.to_param }, session: valid_session
+      user_admin
+      get :edit, params: { id: user_admin.to_param }
       expect(response).to be_success
     end
   end
 
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new user' do
+        expect {
+          post :create, params: { user: valid_attributes } #tengo que definir los valid_attributes?
+        }.to change(User, :count).by(1)
+      end
+      it 'redirects to the created user' do
+        post :create, params: { user: valid_attributes }
+        expect(response).to redirect_to(User.last)
+      end
+    end
+
+    context 'with invalid params' do
+      it 'returns a success response' do
+        post :create, params: { user: invalid_attributes }
+        expect(response).to be_success
+      end
+    end
+  end
+=begin
   describe 'POST #create' do
     context 'with valid params' do
       it 'creates a new User' do
@@ -94,7 +120,31 @@ RSpec.describe UsersController, type: :controller do
       end
     end
   end
+=end
 
+  describe 'PUT #update' do
+    context 'with valid params' do
+      it 'updates the requested user' do
+        user_admin
+        put :update, params: { id: user_admin.to_param, user: valid_attributes }
+        user_admin.reload
+      end
+      it 'redirects to the user' do
+        user_admin
+        put :update, params: { id: user_admin.to_param, user: valid_attributes }
+        expect(response).to redirect_to(user_admin)
+      end
+    end
+
+    context'with invalid params' do
+      it 'returns a success response' do
+        user_admin
+        put :update, params: { id: user_admin.to_param, user: invalid_attributes }
+        expect(response).to be_success
+      end
+    end
+  end
+=begin
   describe 'PUT #update' do
     context 'with valid params' do
       let(:new_attributes) do
@@ -123,19 +173,35 @@ RSpec.describe UsersController, type: :controller do
       end
     end
   end
-
+=end
   describe 'DELETE #destroy' do
     it 'destroys the requested user' do
-      user = User.create! valid_attributes
+      puts "---------------------#{user_admin.inspect}"
+      user_admin
+      expect {
+        delete :destroy, params: { id: user_admin.to_param }
+      }.to change(User, :count).by(-1)
+    end
+    it 'redirects to the users list' do
+      user_admin
+      delete :destroy, params: { id: user_admin.to_param }
+      expect(response).to redirect_to(users_url)
+    end
+  end
+=begin
+  describe 'DELETE #destroy' do
+    it 'destroys the requested user' do
+      user = user_admin
       expect do
         delete :destroy, params: { id: user.to_param }, session: valid_session
       end.to change(User, :count).by(-1)
     end
 
     it 'redirects to the users list' do
-      user = User.create! valid_attributes
+      user = user_admin
       delete :destroy, params: { id: user.to_param }, session: valid_session
       expect(response).to redirect_to(users_url)
     end
   end
+=end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,20 @@
 FactoryGirl.define do
-  factory :user do
-    
+  factory :user_admin, class: User do
+    name "sauron"
+    lastname "dark"
+    email "sauron@gmail.com"
+    role 0
+  end
+  factory :user_boss, class: User do
+    name "legolas"
+    lastname "arrow"
+    email "legolas@gmail.com"
+    role 1
+  end
+  factory :user_employer, class: User do
+    name "frodo"
+    lastname "ring"
+    email "frodo@gmail.com"
+    role 3
   end
 end
