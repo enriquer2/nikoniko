c5cb24e4fe491b4332140a1ca062d3d4520bda65
enriquer2 <ecases@alfatecsistemas.es>
2017-08-10 14:50:52 +0200

    update user policy and user controller for policies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    authorize @users
   end
 
   # GET /users/1
@@ -14,6 +15,7 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
+    authorize @user
   end
 
   # GET /users/1/edit
@@ -23,6 +25,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    authorize @user
 
     respond_to do |format|
       if @user.save
@@ -64,6 +67,7 @@ class UsersController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
+    authorize @user
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,19 +1,31 @@
 # frozen_string_literal: true
 
 class UserPolicy < ApplicationPolicy
+  class Scope
+    def resolve
+      if user.admin? || user.boss?
+        scope.all
+      elsif user.teamleader?
+        scope.where(role: User.roles[:teamleader])
+      else
+        scope.where(role: User.roles[:employer])
+      end
+    end
+  end
+
   def index?
     true
   end
 
-  def create? # duda
+  def create? # este ya no haria falta si hemos puesto que tiene que estar registrado siempre, no?
     user_signed_in?
   end
 
   def update? # modificar usuario si esta registrado y es boss
-    return true if user_signed_in? && current_user.role == 1
+    return true if user_signed_in? && (user.admin? || user.boss?)
   end
 
   def destroy? # destruir usuario si esta registrado y es boss o jefe de equipo
-    return true if user_signed_in? && (current_user.role == 1 || current_user.role == 2)
+    return true if user_signed_in? && (user.admin? || user.boss? == 1 || user.teamleader? == 2)
   end
 end
