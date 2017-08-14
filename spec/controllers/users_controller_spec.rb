2b62abcaab6968af57cd074928ca63054f0c2dee
enriquer2 <ecases@alfatecsistemas.es>
2017-08-14 14:04:53 +0200

    update test for user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe UsersController, type: :controller do
   # UsersController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  shared_examples 'http verbs test' do
+  shared_examples 'user fully authorized' do
     before do
       @user = controller.current_user
     end
@@ -130,24 +130,111 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
+  shared_examples 'user as employee authorized' do
+    before do
+      @user = controller.current_user
+    end
+    describe 'GET #index' do
+      it 'returns a success response' do
+        @user
+        get :index
+        expect(response).to be_success
+      end
+    end
+
+    describe 'GET #show' do
+      it 'returns a success response' do
+        @user
+        get :show, params: { id: @user.to_param }
+        expect(response).to be_success
+      end
+    end
+
+    describe 'GET #new' do
+      it 'not allow to return a success response' do
+        get :new
+        expect(response).to redirect_to(users_url)
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'returns a success response' do
+        @user
+        get :edit, params: { id: @user.to_param }
+        expect(response).to be_success
+      end
+    end
+
+    describe 'POST #create' do
+      context 'with valid params' do
+        it 'not allow to create a new user' do
+          expect do
+            post :create, params: { user: valid_attributes }
+          end.to change(User, :count).by(0)
+        end
+        it 'redirects to the users list' do
+          post :create, params: { user: valid_attributes }
+          expect(response).to redirect_to(users_url)
+        end
+      end
+    end
+
+    describe 'PUT #update' do
+      context 'with valid params' do
+        it 'updates the requested user' do
+          @user
+          put :update, params: { id: @user.to_param, user: valid_attributes }
+          @user.reload
+        end
+        it 'redirects to the user' do
+          @user
+          put :update, params: { id: @user.to_param, user: valid_attributes }
+          expect(response).to redirect_to(@user)
+        end
+      end
+
+      context'with invalid params' do
+        it 'returns a success response' do
+          @user
+          put :update, params: { id: @user.to_param, user: invalid_attributes }
+          expect(response).to be_success
+        end
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'not allow to destroy the requested user' do
+        @user
+        expect do
+          delete :destroy, params: { id: @user.to_param }
+        end.to change(User, :count).by(0)
+      end
+      it 'redirects to the users list' do
+        @user
+        delete :destroy, params: { id: @user.to_param }
+        expect(response).to redirect_to(users_url)
+      end
+    end
+  end
+
   context 'with user admin' do
     # be login to execute test
     login_admin
-    it_behaves_like 'http verbs test'
+    it_behaves_like 'user fully authorized'
   end
 
   context 'with user boss' do
     login_boss
-    it_behaves_like 'http verbs test'
+    it_behaves_like 'user fully authorized'
   end
 
   context 'with user teamleader' do
     login_teamleader
-    it_behaves_like 'http verbs test'
+    it_behaves_like 'user fully authorized'
   end
 
   context 'with user employee' do
     login_employee
-    it_behaves_like 'http verbs test'
+    it_behaves_like 'user as employee authorized'
   end
 end
