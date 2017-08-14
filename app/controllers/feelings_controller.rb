8ab99c0a91fb8e96077c688d0fb6495065bec4a4
enriquer2 <ecases@alfatecsistemas.es>
2017-08-14 10:38:31 +0200

    remove line 'frozen_string...'

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class FeelingsController < ApplicationController
   before_action :set_feeling, only: %i[show edit update destroy]
 
--- db/migrate/20170807112128_change_role_to_users.rb
+++ db/migrate/20170807112128_change_role_to_users.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ChangeRoleToUsers < ActiveRecord::Migration[5.1]
   def up
     change_column :users, :role, :integer, default: 3
