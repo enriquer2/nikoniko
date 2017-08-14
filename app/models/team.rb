9e20460fdf8d683496ea284acd8224ce077de6ac
enriquer2 <ecases@alfatecsistemas.es>
2017-08-14 10:13:10 +0200

    update team and user models

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
 class Team < ApplicationRecord
   belongs_to :user
-
+  has_many :user
   validates :name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :role, presence: true
 
-  enum user_roles: { admin: 0, boss: 1, teamleader: 2, employee: 3 }
+  enum roles: { admin: 0, boss: 1, teamleader: 2, employee: 3 }
 
   protected
 
   def set_default_value
-    role = self.role || 3
+    role = self.role || employee
   end
 end
