af99cb4c1e0788d29467db00b0bf6e6151554a4d
enriquer2 <ecases@alfatecsistemas.es>
2017-08-17 12:44:38 +0200

    create request login for tests

---
+++ spec/support/request_users.rb
@@ -0,0 +1,12 @@
+module RequestUsers
+  def login_admin
+    before do
+      @current_user = create(:admin)
+      post user_session_path, params: {
+        'user[email]' => @current_user.email,
+        'user[password]' => @current_user.password
+      }
+      follow_redirect!
+    end
+  end
+end
