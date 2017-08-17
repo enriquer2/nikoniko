c6771056726bd6f00ce1e41da79aa4f135ba308d
enriquer2 <ecases@alfatecsistemas.es>
2017-08-17 12:42:14 +0200

    delete useless file

--- spec/support/request_policies.rb
+++
@@ -1,9 +0,0 @@
-module RequestPolicy
-  def login_admin
-    before(:each) do
-      @request.env['devise.mapping'] = Devise.mappings[:user]
-      user = create(:admin)
-      sign_in user
-    end
-  end
-end
