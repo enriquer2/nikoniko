cc5c8efc8e6dc7993f8590e63ba0111c2b42e43c
enriquer2 <ecases@alfatecsistemas.es>
2017-08-17 16:54:04 +0200

    correct team factories

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,10 +1,5 @@
 FactoryGirl.define do
   factory :team do
     name { Faker::Name.last_name }
-
-    factory :team1, class: Team do
-    end
-    factory :team2, class: Team do
-    end
   end
 end
