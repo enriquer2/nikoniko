dd1d5bb413ab28856950f4e5a4034723dae5f8b8
enriquer2 <ecases@alfatecsistemas.es>
2017-08-18 13:41:06 +0200

    fix error with password lenght

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,27 @@ admin = Team.create({name: 'admin'})
 boss = Team.create({name: 'boss'})
 teamleader = Team.create({name: 'teamleader'})
 employee = Team.create({name: 'employee'})
-User.create({ name: 'admin', lastname: 'admin', email: 'admin@gmail.com', password: 'admin', role: 'admin', team_id: admin.id })
-User.create({ name: 'boss', lastname: 'boss', email: 'boss@gmail.com', password: 'boss', role: 'boss', team_id: boss.id })
-User.create({ name: 'teamleader', lastname: 'teamleader', email: 'teamleader@gmail.com', password: 'teamleader', role: 'teamleader', team_id: teamleader.id })
-User.create({ name: 'employee', lastname: 'employee', email: 'employee@gmail.com', password: 'employee', role: 'employee', team_id: employee.id })
+User.create({ name: 'admin',
+ lastname: 'admin',
+ email: 'admin11@gmail.com',
+ password: 'nikoniko',
+ role: 'admin',
+ team_id: admin.id })
+User.create({ name: 'boss',
+ lastname: 'boss',
+ email: 'boss11@gmail.com',
+ password: 'nikoniko',
+ role: 'boss',
+ team_id: boss.id })
+User.create({ name: 'teamleader',
+ lastname: 'teamleader',
+ email: 'teamleader@gmail.com',
+ password: 'nikoniko',
+ role: 'teamleader',
+ team_id: teamleader.id })
+User.create({ name: 'employee',
+ lastname: 'employee',
+ email: 'employee@gmail.com',
+ password: 'nikoniko',
+ role: 'employee',
+ team_id: employee.id })
