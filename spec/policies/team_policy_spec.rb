b55322050d270ae72c2c84c31eaa3119a43daef0
enriquer2 <ecases@alfatecsistemas.es>
2017-08-21 14:09:44 +0200

    update team policy test for teamleader and fix some syntax errors

--- spec/policies/team_policy_spec.rb
+++ spec/policies/team_policy_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe TeamPolicy do
-  let(:team1) { FactoryGirl.create(:team) }
+  let(:team1) { create(:team) }
+  let(:team2) { create(:team) }
+  let(:team3) { create(:team) }
+  let(:user1) { create(:user, team: team1) }
   shared_examples 'user fully authorized' do
     it { is_expected.to permit_action(:index) }
     it { is_expected.to permit_action(:show) }
@@ -25,30 +28,41 @@ RSpec.describe TeamPolicy do
   shared_examples 'user team authorized' do
     it { is_expected.to permit_action(:index) }
     it { is_expected.to permit_action(:show) }
-    it { is_expected.to permit_action(:create) }
-    it { is_expected.to permit_action(:new) }
+    it { is_expected.not_to permit_action(:create) }
+    it { is_expected.not_to permit_action(:new) }
     it { is_expected.to permit_action(:update) }
     it { is_expected.to permit_action(:edit) }
-    it { is_expected.to permit_action(:destroy) }
+    it { is_expected.not_to permit_action(:destroy) }
   end
 
-  #### Aqui es donde deberia instanciar dos team, una siendo el mismo que he creado arriba
-  #     y otro siendo diferente al de arriba. Por tanto tambien debe tener duplicados del
-  #     test porque tendra comportamiento distinto
+  shared_examples 'user team not authorized' do
+    it { is_expected.not_to permit_action(:index) }
+    it { is_expected.not_to permit_action(:show) }
+    it { is_expected.not_to permit_action(:create) }
+    it { is_expected.not_to permit_action(:new) }
+    it { is_expected.not_to permit_action(:update) }
+    it { is_expected.not_to permit_action(:edit) }
+    it { is_expected.not_to permit_action(:destroy) }
+  end
 
   context 'with user admin' do
-    subject { UserPolicy.new(create(:admin), user) }
+    subject { TeamPolicy.new(create(:admin), team1) }
     # que puede hacer quien sobre que
     it_behaves_like 'user fully authorized'
   end
   context 'with user boss' do
-    subject { UserPolicy.new(create(:boss), user) }
+    subject { TeamPolicy.new(create(:boss), team1) }
     # que puede hacer quien sobre que
     it_behaves_like 'user boss authorized'
   end
-  context 'with user teamleader' do
-    subject { UserPolicy.new(create(:teamleader), user) }
+  context 'with user teamleader over his team' do
+    subject { TeamPolicy.new(create(:teamleader, team: team1), team1) }
     # que puede hacer quien sobre que
     it_behaves_like 'user team authorized'
+  end
+  context 'with user teamleader over other team' do
+    subject { TeamPolicy.new(create(:teamleader, team: team2), team3) }
+    # que puede hacer quien sobre que
+    it_behaves_like 'user team not authorized'
   end
 end
