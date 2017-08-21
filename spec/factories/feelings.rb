7b46454ef979f7a06edabd537f592344fb868f1c
enriquer2 <ecases@alfatecsistemas.es>
2017-08-21 16:56:20 +0200

    add feeling factory

--- spec/factories/feelings.rb
+++ spec/factories/feelings.rb
@@ -1,21 +1,7 @@
 FactoryGirl.define do
-  factory :user do
-    name { Faker::Name.last_name }
-    lastname { Faker::Name.name }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password }
-
-    factory :admin, class: User do
-      role 'admin'
-    end
-    factory :boss, class: User do
-      role 'boss'
-    end
-    factory :teamleader, class: User do
-      role 'teamleader'
-    end
-    factory :employee, class: User do
-      role 'employee'
-    end
+  factory :feeling do
+    # status { Faker::Number.between(1, 5) }
+    status { Feeling.statuses.values.sample}
+    day { Faker::Date.between(3.days.ago, Date.today) }
   end
 end
