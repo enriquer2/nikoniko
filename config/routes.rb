9c32e064f5e9b0725791202e1bedd7831bb15871
enriquer2 <ecases@alfatecsistemas.es>
2017-08-23 16:55:28 +0200

    create a new route for welcome

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :teams
   resources :feelings
   resources :users
+  resources :welcome, only: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
 end
