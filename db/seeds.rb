37e8b7a0dbc4bc96f841248cae3dfebe3771bdfc
enriquer2 <ecases@alfatecsistemas.es>
2017-08-23 14:43:49 +0200

    update seeds file with some feelings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,44 @@
-admin = Team.create({name: 'admin'})
-boss = Team.create({name: 'boss'})
-teamleader = Team.create({name: 'teamleader'})
-employee = Team.create({name: 'employee'})
-User.create({ name: 'admin',
+t_admin = Team.create({name: 'admin'})
+t_boss = Team.create({name: 'boss'})
+t_teamleader = Team.create({name: 'teamleader'})
+t_employee = Team.create({name: 'employee'})
+u1 = User.create({ name: 'admin',
  lastname: 'admin',
  email: 'admin11@gmail.com',
  password: 'nikoniko',
  role: 'admin',
- team_id: admin.id })
-User.create({ name: 'boss',
+ team_id: t_admin.id })
+u2 = User.create({ name: 'boss',
  lastname: 'boss',
  email: 'boss11@gmail.com',
  password: 'nikoniko',
  role: 'boss',
- team_id: boss.id })
-User.create({ name: 'teamleader',
+ team_id: t_boss.id })
+u3 = User.create({ name: 'teamleader',
  lastname: 'teamleader',
  email: 'teamleader@gmail.com',
  password: 'nikoniko',
  role: 'teamleader',
- team_id: teamleader.id })
-User.create({ name: 'employee',
+ team_id: t_teamleader.id })
+u4 = User.create({ name: 'employee',
  lastname: 'employee',
  email: 'employee@gmail.com',
  password: 'nikoniko',
  role: 'employee',
- team_id: employee.id })
+ team_id: t_employee.id })
+Feeling.create({
+  day: '2017/8/23',
+  status: 'heaven',
+  user: u1 })
+Feeling.create({
+  day: '2017/8/23',
+  status: 'frustrated',
+  user: u2 })
+Feeling.create({
+  day: '2017/8/23',
+  status: 'purgatory',
+  user: u3 })
+Feeling.create({
+  day: '2017/8/23',
+  status: 'satisfied',
+  user: u4 })
