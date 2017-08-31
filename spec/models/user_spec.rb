a355f8c9556346a8e5ea495407c51c937df24660
enriquer2 <ecases@alfatecsistemas.es>
2017-08-31 15:01:24 +0200

    remove useless code repeated

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,17 +3,3 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { expect(User.new.role).to eq('employee') }
 end
-
-describe User do
-  it 'Should have role employee as default' do
-    user = User.new
-    expect(user.role).to eq('employee')
-  end
-end
-
-describe User do
-  it 'Should have role 3 as default' do
-    user = User.new
-    expect(user.role).to eq(3)
-  end
-end
